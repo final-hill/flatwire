c47bda7e8e07d1aca5709b066a97718aec82d899
mlhaufe <tno@thenewobjective.com>
2020-09-30 19:20:56 +0000

    Deploying to gh-pages from  @ 15e3d84d1d9934c46cd3ebd998cb8f8c1d8c074a 🚀

---
+++ precache-manifest.0c6232b4f915ca729a9e399559bb2ba7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71484ff0be48ccd37986",
+    "url": "/css/app.26071d38.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e0199efee2f634a54cf26bb54897e49e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be49ad1b346c121ff3a7",
+    "url": "/js/about.42dc6d9e.js"
+  },
+  {
+    "revision": "71484ff0be48ccd37986",
+    "url": "/js/app.be59bb8d.js"
+  },
+  {
+    "revision": "003d48ddcb6bc01acef2",
+    "url": "/js/chunk-vendors.b6d214ed.js"
+  },
+  {
+    "revision": "0a31b9fe8960f5f55abb1bc710bbfb93",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
