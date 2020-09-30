f7ec5365db5e5196000df5685fdfcddcf6795294
Michael Haufe <tno@thenewobjective.com>
2020-09-30 15:29:17 -0500

    added vue config

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // productionTip: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/standing-echo/'
+    : '/',
+  pwa: {
+    manifestPath: 'manifest.webmanifest',
+    name: 'Flatwire',
+    themeColor: '#000000',
+    msTileColor: '#000000'
+  }
+}
